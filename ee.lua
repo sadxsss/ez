89c970fc230153c4cca46bcd21614aa7081774cc
sadxsss <lzottozol@gmail.com>
2024-10-11 16:35:53 +0700

    Update ee.lua

--- ee.lua
+++ ee.lua
@@ -1,4 +1,4 @@
 return {
-  ['Enabled'] = false, -- เปิดเพื่อให้กลับ Lobby ละรอ 5-6 นาที
-  ['Roll'] = false, -- true เพื่อสุ่มตัว
+  ['Enabled'] = true, -- เปิดเพื่อให้กลับ Lobby ละรอ 5-6 นาที
+  ['Roll'] = true, -- true เพื่อสุ่มตัว
 };
